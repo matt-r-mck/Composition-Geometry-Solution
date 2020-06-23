3db661531a20dd0af7a92d6069b2f9c670e83efc
matt-r-mck <matt.r.mck@gmail.com>
2020-06-23 10:51:56 -0400

    Add project files.

---
+++ CompositionGeometry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CompositionGeometry {
+    class Program {
+        static void Main(string[] args) {
+
+
+            var quad1 = new Quadrillateral {
+                Leg1 = 2, Leg2 = 4, Leg3 = 6, Leg4 = 8
+            };
+            quad1.GetPerimeter();
+
+            var rect1 = new Rectangle(5, 7) {};
+            rect1.GetPerimeter();
+            rect1.GetArea();
+
+            var sq1 = new Square(7) {};
+            sq1.GetPerimeter();
+            sq1.GetArea();
+
+        }
+    }
+}
---
+++ CompositionGeometry/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositionGeometry {
+    public class Rectangle { //does not inherit quad
+
+        private Quadrillateral quad = new Quadrillateral(); // creates a var quad type Quadrillateral
+
+        public void GetPerimeter() {
+            quad.GetPerimeter();
+        }
+
+        public void GetArea() {
+            Console.WriteLine($"The Area is {quad.Leg1 * quad.Leg2}"); 
+        }
+
+        public Rectangle(int l1, int l2) { //takes user entered parameters, sets them to egs
+            quad.Leg1 = l1;
+            quad.Leg3 = l1;
+            quad.Leg2 = l2;
+            quad.Leg4 = l2;
+        }
+
+    }
+}
